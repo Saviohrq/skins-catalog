8728c8767b191d30d9297b334adb50966623f050
Savio Henrique <saviobarradas@gmail.com>
2025-10-23 17:55:12 -0300

    feat(theme): Define paleta de cores centralizada

---
+++ src/constants/colors.ts
@@ -0,0 +1,9 @@
+export const Cores = {
+  fundo: '#1C1C1E',
+  fundoCard: '#2C2C2E',
+  fundoBotao: '#3A3A3C',
+  textoPrincipal: '#FFFFFF',
+  textoSecundario: '#AEAEB2',
+  acentoPrincipal: '#0A84FF',
+  acentoSecundario: '#30a1d1ff',
+};
